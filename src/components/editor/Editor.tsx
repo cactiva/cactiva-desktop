aa92a8b7aaaa4db8b5685ab8b7e473334ff120bd
Rizky <rizky@andromedia.co.id>
2020-02-19 17:23:08 +0700

    fix

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -2,9 +2,19 @@ import React from 'react';
 import { observer } from 'mobx-react-lite';
 import './Editor.scss';
 import FileTree from './filetree/FileTree';
+import Splitter from 'm-react-splitters';
+import 'm-react-splitters/lib/splitters.css';
 
 export default observer(() => {
     return <div className='editor'>
-        <FileTree />
+        <Splitter
+            position="vertical"
+            primaryPaneMinWidth="200px"
+            primaryPaneWidth="250px"
+            postPoned={false}
+        >
+            <FileTree />
+            <div></div>
+        </Splitter>
     </div>;
 });
